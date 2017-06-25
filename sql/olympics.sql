f644d637c2ddf9b189257c5940584e0fbd6c8d69
giovanna <giovannaguimaraes44@gmail.com>
2017-06-25 02:27:45 -0300

    Added foreign keys

--- sql/olympics.sql
+++ sql/olympics.sql
@@ -4,42 +4,49 @@ CREATE TABLE Pessoa (
 	Estado NOT NULL,
 	Pais NOT NULL,
 	Sexo NOT NULL,
-	DataNascimento (fk) NOT NULL 
+	FOREIGN KEY (DataNascimento) NOT NULL,
+	FOREIGN KEY (DataNascimento) REFERENCES Data(Dia, Mes, Ano) 
 );
 CREATE TABLE Preparador (
-	PRIMARY KEY(Pessoa) (fk) NOT NULL,
+	PRIMARY KEY(Pessoa) NOT NULL,
+	FOREIGN KEY(Pessoa) REFERENCES Pessoa(Passaporte),
 	Email NOT NULL,
 	Senha NOT NULL 
 );
 CREATE TABLE Atleta (
-	PRIMARY KEY(Pessoa) (fk) NOT NULL,
+	PRIMARY KEY(Pessoa) NOT NULL,
+	FOREIGN KEY(Pessoa) REFERENCES Pessoa(Passaporte),
 	Peso NOT NULL,
 	Altura NOT NULL,
 	Regularidade NOT NULL,
 	NPunicoes NOT NULL,
-	Preparador (fk) NOT NULL,
-	Modalidade (fk) NOT NULL,
-	Nacao (fk) NOT NULL 
+	FOREIGN KEY(Preparador) NOT NULL,
+	FOREIGN KEY(Preparador) REFERENCES Preparador(Passaporte),
+	FOREIGN KEY(Modalidade) NOT NULL,
+	FOREIGN KEY(Modalidade) REFERENCES Modalidade(IDModalidade),
+	FOREIGN KEY(Nacao) NOT NULL,
+	FOREIGN KEY(Nacao) REFERENCES Nacao(NomeNacao) 
 );
 CREATE TABLE TelefonePessoa (
-	PRIMARY KEY(Pessoa) (fk) NOT NULL,
-	PRIMARY KEY(Telefone) (fk) NOT NULL 
+	PRIMARY KEY(Pessoa) NOT NULL,
+	FOREIGN KEY(Pessoa) REFERENCES Pessoa(Passaporte),
+	PRIMARY KEY(Telefone) NOT NULL
 );
 CREATE TABLE TelefoneMedico (
-	PRIMARY KEY(Medico) (fk) NOT NULL,
-	PRIMARY KEY(Telefone) (fk) NOT NULL 
+	PRIMARY KEY(Medico) NOT NULL,
+	FOREIGN KEY(Medico) REFERENCES Medico(CRM),
+	PRIMARY KEY(Telefone) NOT NULL,
+	FOREIGN KEY(Telefone) REFERENCES Telefone(Pessoa, Telefone) 
 );
 CREATE TABLE RotinaTreino (
 	PRIMARY KEY(IDRotina) NOT NULL,
-	PRIMARY KEY(Preparador) (fk) NOT NULL,
+	PRIMARY KEY(Preparador) NOT NULL,
+	FOREIGN KEY(Preparador) REFERENCES Preparador(Passaporte),
 	Duracao NOT NULL 
 );
-CREATE TABLE PreparadorRotina (
-	PRIMARY KEY(RotinaTreino) (fk) NOT NULL,
-	PRIMARY KEY(Preparador) (fk) NOT NULL 
-);
 CREATE TABLE DiasTreino (
-	PRIMARY KEY(Rotina) (fk) NOT NULL,
+	PRIMARY KEY(RotinaTreino) NOT NULL,
+	FOREIGN KEY(RotinaTreino) REFERENCES RotinaTreino(IDRotina, Preparador),
 	PRIMARY KEY(DiaSemana) NOT NULL 
 );
 CREATE TABLE Preparo (
@@ -55,16 +62,22 @@ CREATE TABLE Treino (
 	DescricaoTreino NOT NULL 
 );
 CREATE TABLE PreparoRotina (
-	PRIMARY KEY(RotinaTreino) (fk) NOT NULL,
-	PRIMARY KEY(Preparo) (fk) NOT NULL 
+	PRIMARY KEY(RotinaTreino) NOT NULL,
+	FOREIGN KEY(RotinaTreino) REFERENCES RotinaTreino(IDRotina, Preparador),
+	PRIMARY KEY(Preparo) NOT NULL,
+	FOREIGN KEY(Preparo) REFERENCES Preparo(IDPreparo) 
 );
 CREATE TABLE RecuperacaoRotina (
-	PRIMARY KEY(RotinaTreino) (fk) NOT NULL,
-	PRIMARY KEY(Recuperacao) (fk) NOT NULL 
+	PRIMARY KEY(RotinaTreino) NOT NULL,
+	FOREIGN KEY(RotinaTreino) REFERENCES RotinaTreino(IDRotina, Preparador),
+	PRIMARY KEY(Recuperacao) NOT NULL,
+	FOREIGN KEY(Recuperacao) REFERENCES Recuperacao(IDRecuperacao)
 );
 CREATE TABLE TreinoRotina (
-	PRIMARY KEY(RotinaTreino) (fk) NOT NULL,
-	PRIMARY KEY(Treino) (fk) NOT NULL 
+	PRIMARY KEY(RotinaTreino) NOT NULL,
+	FOREIGN KEY(RotinaTreino) REFERENCES RotinaTreino(IDRotina)
+	PRIMARY KEY(Treino) NOT NULL
+	FOREIGN KEY(Treino) REFERENCES Treino(IDTreino) 
 );
 CREATE TABLE Modalidade (
 	PRIMARY KEY(IDModalidade) NOT NULL,
@@ -88,14 +101,16 @@ CREATE TABLE Medico (
 	Pais NOT NULL 
 );
 CREATE TABLE TestarDoping (
-	PRIMARY KEY(Medico) (fk) NOT NULL,
-	PRIMARY KEY(Atleta) (fk) NOT NULL,
-	PRIMARY KEY(TesteDoping) (fk) NOT NULL 
+	PRIMARY KEY(Medico) NOT NULL,
+	FOREIGN KEY(Medico) REFERENCES Medico(CRM),
+	PRIMARY KEY(Atleta) NOT NULL,
+	FOREIGN KEY(Atleta) REFERENCES Atleta(Passaporte),
+	PRIMARY KEY(TesteDoping) NOT NULL,
+	FOREIGN KEY(TesteDoping) REFERENCES TesteDoping(IDTeste)
 );
 CREATE TABLE Diagnostico (
 	PRIMARY KEY(IDDiagnostico) NOT NULL,
-	DescricaoDiagnostico NOT NULL,
-	MetodoTratamento (fk) NOT NULL 
+	DescricaoDiagnostico NOT NULL
 );
 CREATE TABLE MetodoTratamento (
 	PRIMARY KEY(IDMetodo) NOT NULL,
@@ -103,45 +118,61 @@ CREATE TABLE MetodoTratamento (
 	DescricaoEfetividade 
 ); 
 CREATE TABLE Atendimento (
-	PRIMARY KEY(Medico) (fk) NOT NULL,
-	PRIMARY KEY(Atleta) (fk) NOT NULL,
-	PRIMARY KEY(DataConsulta) (fk) NOT NULL,
-	PRIMARY KEY(MedicoConsulta) (fk) NOT NULL,
-	PRIMARY KEY(AtletaConsulta) (fk) NOT NULL 
+	PRIMARY KEY(MedicoConsulta) NOT NULL,
+	FOREIGN KEY(MedicoConsulta) REFERENCES Consulta(Medico),
+	PRIMARY KEY(AtletaConsulta) NOT NULL,
+	FOREIGN KEY(AtletaConsulta) REFERENCES Consulta(Atleta),
+	PRIMARY KEY(DataConsulta) NOT NULL,
+	FOREIGN KEY(DataConsulta) REFERENCES Data(Dia, Mes, Ano)
 ); 
 CREATE TABLE Tratamento (
-	PRIMARY KEY(Diagnostico) (fk) NOT NULL,
-	PRIMARY KEY(MetodoTratamento) (fk) NOT NULL 
+	PRIMARY KEY(Diagnostico) NOT NULL,
+	FOREIGN KEY(Diagnostico) REFERENCES Diagnostico(IDDiagnostico),
+	PRIMARY KEY(MetodoTratamento) NOT NULL,
+	FOREIGN KEY(MetodoTratamento) REFERENCES MetodoTratamento(IDMetodo) 
 );
 CREATE TABLE Lesao (
 	PRIMARY KEY(IDLesao) NOT NULL,
 	Descricao NOT NULL 
 );
 CREATE TABLE LesaoMedico (
-	PRIMARY KEY(Lesao) (fk) NOT NULL,
-	PRIMARY KEY(Medico) (fk) NOT NULL 
+	PRIMARY KEY(Lesao) NOT NULL,
+	FOREIGN KEY(Lesao) REFERENCES Lesao(IDLesao),
+	PRIMARY KEY(Medico) NOT NULL,
+	FOREIGN KEY(Medico) REFERENCES Medico(CRM)
 );
 CREATE TABLE LesaoAtleta (
-	PRIMARY KEY(Lesao) (fk) NOT NULL,
-	PRIMARY KEY(Atleta) (fk) NOT NULL 
+	PRIMARY KEY(Lesao) NOT NULL,
+	FOREIGN KEY(Lesao) REFERENCES Lesao(IDLesao),
+	PRIMARY KEY(Atleta) NOT NULL,
+	FOREIGN KEY(Atleta) REFERENCES Atleta(Passaporte)
 );
 CREATE TABLE TesteDoping (
 	PRIMARY KEY(IDTeste) NOT NULL,
-	Data (fk) NOT NULL,
+	FOREIGN KEY(Data) NOT NULL,
+	FOREIGN KEY(Data) REFERENCES Data(Dia, Mes, Ano),
 	Descricao,
 	Resultado NOT NULL 
 ); 
 CREATE TABLE Consulta (
-	PRIMARY KEY(Data) (fk) NOT NULL,
-	PRIMARY KEY(Atleta) (fk) NOT NULL,
-	PRIMARY KEY(Medico) (fk) NOT NULL,
+	FOREIGN KEY(Data) NOT NULL,
+	FOREIGN KEY(Data) REFERENCES Data(Dia, Mes, Ano),
+	PRIMARY KEY(Atleta) NOT NULL,
+	FOREIGN KEY(Atleta) REFERENCES Atleta(Passaporte),
+	PRIMARY KEY(Medico) NOT NULL,
+	FOREIGN KEY(Medico) REFERENCES Medico(CRM),
 	DescricaoConsulta,
-	Diagnostico (fk) NOT NULL 
+	Diagnostico NOT NULL,
+	PRIMARY KEY(Diagnostico) NOT NULL,
+	FOREIGN KEY(Diagnostico) REFERENCES Diagnostico(IDDiagnostico)
 ); 
 CREATE TABLE Sintoma (
-	PRIMARY KEY(DataConsulta) (fk) NOT NULL,
-	PRIMARY KEY(MedicoConsulta) (fk) NOT NULL,
-	PRIMARY KEY(AtletaConsulta) (fk) NOT NULL,
+	PRIMARY KEY(DataConsulta) NOT NULL,
+	FOREIGN KEY(DataConsulta) REFERENCES Consulta(Data),
+	PRIMARY KEY(MedicoConsulta) NOT NULL,
+	FOREIGN KEY(MedicoConsulta) REFERENCES Consulta(Medico),
+	PRIMARY KEY(AtletaConsulta) NOT NULL,
+	FOREIGN KEY(MedicoConsulta) REFERENCES Consulta(Atleta),
 	PRIMARY KEY(Sintoma) NOT NULL 
 );
 CREATE TABLE Data (
