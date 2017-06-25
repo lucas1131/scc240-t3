ab54cac81bead215778dbc79ba43e8cf16b71da6
Lucas Alexandre Soares <lucassoares1793@gmail.com>
2017-06-24 21:12:05 -0300

    add NetBeans compatible

--- app/src/app/main.java
+++ app/src/app/main.java
@@ -19,7 +19,7 @@ public class main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+asdas        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         
         Scene scene = new Scene(root);
         
