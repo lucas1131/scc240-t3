7cd6b5a973f83c7f4b7edd991530da9678352e0f
rafaelmonteiro95 <rafaelmonteiro95@gmail.com>
2017-06-24 21:56:25 -0300

    tentando arrumar o merge

--- app/src/app/main.java
+++ app/src/app/main.java
@@ -19,7 +19,7 @@ public class main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/MainWindow.fxml"));
         
         Scene scene = new Scene(root);
         
