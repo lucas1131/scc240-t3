f4d769b2efaa11b69038d719dc00e1b2d873a724
José Augusto Noronha de Menezes Neto <jaugustonmn@gmail.com>
2017-12-12 02:08:33 -0200

    Pipipopulação
    Populei esta merda

---
+++ sql/pipipopulatetables.sql
@@ -0,0 +1,60 @@
+INSERT INTO Genero VALUES('Comédia');
+INSERT INTO Genero VALUES('Terror');
+INSERT INTO Genero VALUES('Romance');
+INSERT INTO Genero VALUES('Ficção Científica');
+INSERT INTO Genero VALUES('Suspense');
+
+INSERT INTO Numero_do_Cartao_de_Credito VALUES('1111111111111111','ELO','123','11/09/2001','Gebariel Nacismento');
+INSERT INTO Numero_do_Cartao_de_Credito VALUES('2222222222222222','CIELO','456','29/04/1994','José Otávio Edmilson');
+INSERT INTO Numero_do_Cartao_de_Credito VALUES('3333333333333333','VISAPAY','789','24/12/2017','Letícia Tina Sakurai');
+INSERT INTO Numero_do_Cartao_de_Credito VALUES('4444444444444444','MASTERCARD','345','15/12/2017','Otávio Luis Aguiar');
+INSERT INTO Numero_do_Cartao_de_Credito VALUES('5555555555555555','SODEXO','666','30/012018','Airo Soares');
+
+INSERT INTO Conta_do_Deposito_Bancario VALUES('354670','12345','Banco do Brasil','Victor','Luis Forbes','0051314517');
+INSERT INTO Conta_do_Deposito_Bancario VALUES('684540','35467','Bradesco','José Augusto','Noronha','05880216527');
+INSERT INTO Conta_do_Deposito_Bancario VALUES('135665','68454','Santander','Valéria','Maia Lobo Menezes','00498289599');
+INSERT INTO Conta_do_Deposito_Bancario VALUES('490207','12213','City Bank','Roberta','Tonello','05880236551');
+INSERT INTO Conta_do_Deposito_Bancario VALUES('133441','25478','Caixa Econômica','Marcelo','Manzato','00736512345');
+
+INSERT INTO Idade_Faixa_Etaria VALUES('25','18');
+INSERT INTO Idade_Faixa_Etaria VALUES('11','14');
+INSERT INTO Idade_Faixa_Etaria VALUES('34','18');
+INSERT INTO Idade_Faixa_Etaria VALUES('19','16');
+INSERT INTO Idade_Faixa_Etaria VALUES('15','10');
+
+INSERT INTO Pessoa VALUES('Gebariel Nacismento');
+INSERT INTO Pessoa VALUES('José Otávio Edmilson');
+INSERT INTO Pessoa VALUES('Letícia Tina Sakurai');
+INSERT INTO Pessoa VALUES('Otávio Luis Aguiar');
+INSERT INTO Pessoa VALUES('Airo Soares');
+
+INSERT INTO Serie VALUES('Gebariel Nacismento','3','18');
+INSERT INTO Serie VALUES('José Otávio Edmilson','1','18');
+INSERT INTO Serie VALUES('Letícia Tina Sakurai','4','10');
+INSERT INTO Serie VALUES('Otávio Luis Aguiar','2','16');
+INSERT INTO Serie VALUES('Airo Soares','8','10');
+
+INSERT INTO Dispositivo VALUES('1','Gebariel Nacismento','Android');
+INSERT INTO Dispositivo VALUES('2','José Otávio Edmilson','IOS');
+INSERT INTO Dispositivo VALUES('3','Letícia Tina Sakurai','Android');
+INSERT INTO Dispositivo VALUES('4','Otávio Luis Aguiar','MAC OS');
+INSERT INTO Dispositivo VALUES('5','Airo Soares','Windows');
+
+INSERT INTO Review VALUES('1','6','25/10/2016','Série muito boa! Melhor que Globo!','25/10/2016');
+INSERT INTO Review VALUES('2','5','13/12/2013','Melhor que já vi!','16/12/2003');
+INSERT INTO Review VALUES('3','8','05/04/2005','Atores de primeira!','20/05/2004');
+INSERT INTO Review VALUES('4','10','18/06/2001','Roteiros muito bem escritos.','19/06/2001');
+INSERT INTO Review VALUES('5','7','22/04/1996','Fraca. Esperava mais.','22/05/1997');
+
+INSERT INTO Temporada_Midia VALUES('Friends','10');
+INSERT INTO Temporada_Midia VALUES('How I Met Your Mother','8');
+INSERT INTO Temporada_Midia VALUES('Stranger Things','2');
+INSERT INTO Temporada_Midia VALUES('How to Get Away with Murder','4');
+INSERT INTO Temporada_Midia VALUES('Lost','6');
+
+INSERT INTO Perfil_Assiste_Midia VALUES('leticiarina','leticia_rina','Harry Potter and the Half-Blood Prince','1','1456');
+INSERT INTO Perfil_Assiste_Midia VALUES('mrpinoquio','mr_pinoquio','The Breakfast Club','2','2735');
+INSERT INTO Perfil_Assiste_Midia VALUES('juregmonteiro','jureg_monteiro','Clube da Luta','3','1523');
+INSERT INTO Perfil_Assiste_Midia VALUES('kawaiiro','kawaiiro','Titanic','4','1233');
+INSERT INTO Perfil_Assiste_Midia VALUES('gguimaraes','g_guimaraes','Moonlight','5','1822');
+
