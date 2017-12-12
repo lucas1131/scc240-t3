72fb985c985be79e42e63adb29237128b62c84b4
Lucas Soares <lucassoares1793@gmail.com>
2017-12-12 16:31:06 -0200

    Falta popupar

---
+++ sql/falta-popular2.sql
@@ -0,0 +1,14 @@
+Audio_Midia;
+Cartao_de_Credito;
+Conta_Deposito_Bancario;
+Deposito_Bancario;
+Gerencia;
+Legenda_Midia;
+Midia;
+Midia_Pertence_Genero;
+Midia_Pertence_Temporada;
+Numero_Cartao_de_Credito;
+Paypal;
+Perfil_Prefere_Genero;
+Perfil_Solicita_Amizade;
+Publica;
