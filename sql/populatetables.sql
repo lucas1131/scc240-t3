461890ca4a2b20d8fd9708bd2f4d26cb0f66cc90
Leticia Rina Sakurai <leticialrsakurai@gmail.com>
2017-12-12 00:20:22 -0200

    População de algumas tabelas

--- sql/populatetables.sql
+++ sql/populatetables.sql
@@ -1,160 +1,60 @@
+INSERT INTO Acesso VALUES ('25-JUN-2015 09:23:50', 'Zenfone 5', 'leticiarina', '192.168.1.2');
+INSERT INTO Acesso VALUES ('10-SET-2016 00:19:23', 'Galaxy S3', 'mrpinoquio', '127.183.4.5');
+INSERT INTO Acesso VALUES ('02-JAN-2017 18:01:35', 'LG KP215', 'kawaiiro', '193.156.8.2');
+INSERT INTO Acesso VALUES ('17-NOV-2014 15:57:00', 'iPhone 5C', 'gguimaraes', '174.185.4.1');
+INSERT INTO Acesso VALUES ('21-MAI-2017 22:48:18', 'Dell Inspiron', 'juregmonteiro', '139.177.7.2');
+
+INSERT INTO Perfil_Possui_Dispositivo VALUES ('1', 'leticiarina', 'leticia_rina');
+INSERT INTO Perfil_Possui_Dispositivo VALUES ('2', 'mrpinoquio', 'mr_pinoquio');
+INSERT INTO Perfil_Possui_Dispositivo VALUES ('3', 'kawaiiro', 'kawaiiro');
+INSERT INTO Perfil_Possui_Dispositivo VALUES ('4', 'gguimaraes', 'g_guimaraes');
+INSERT INTO Perfil_Possui_Dispositivo VALUES ('5', 'juregmonteiro', 'jureg_monteiro');
+
+INSERT INTO Amizade VALUES ('1', 'leticiarina', 'leticia_rina', 'mrpinoquio', 'mr_pinoquio');
+INSERT INTO Amizade VALUES ('2', 'mrpinoquio', 'mr_pinoquio', 'juregmonteiro', 'jureg_monteiro');
+INSERT INTO Amizade VALUES ('3', 'kawaiiro', 'kawaiiro', 'gguimaraes', 'g_guimaraes');
+INSERT INTO Amizade VALUES ('4', 'juregmonteiro', 'jureg_monteiro', 'kawaiiro', 'kawaiiro');
+INSERT INTO Amizade VALUES ('5', 'gguimaraes', 'g_guimaraes', 'leticiarina', 'leticia_rina');
 
 
-INSERT INTO Pessoa VALUES ('123cdxPP', 'Airo preparador', 'Guarulhos', 'São Paulo', 'Brasil', 'M', '13-DEC-1996');
-INSERT INTO Pessoa VALUES ('321xdcPP', 'Gi preparador', 'Ribeirão Preto', 'São Paulo', 'Brasil', 'F', '28-DEC-1996');
-INSERT INTO Pessoa VALUES ('70r741PP', 'Jurg preparador', 'São Paulo', 'São Paulo', 'Brasil', 'M', '26-NOV-1995');
-INSERT INTO Pessoa VALUES ('123cdxAA', 'Airo atleta', 'Guarulhos', 'São Paulo', 'Brasil', 'M', '13-DEC-1996');
-INSERT INTO Pessoa VALUES ('321xdcAA', 'Gi atleta', 'Ribeirão Preto', 'São Paulo', 'Brasil', 'F', '28-DEC-1996');
-INSERT INTO Pessoa VALUES ('70r741AA', 'Jurg atleta', 'São Paulo', 'São Paulo', 'Brasil', 'M', '26-NOV-1995');
-INSERT INTO Pessoa VALUES ('623cdxAA', 'Airo2 atleta', 'Guarulhos', 'São Paulo', 'Brasil', 'M', '13-DEC-1996');
-INSERT INTO Pessoa VALUES ('621xdcAA', 'Gi2 atleta', 'Ribeirão Preto', 'São Paulo', 'Brasil', 'F', '28-DEC-1996');
-INSERT INTO Pessoa VALUES ('60r741AA', 'Jurg2 atleta', 'São Paulo', 'São Paulo', 'Brasil', 'M', '26-NOV-1995');
+INSERT INTO Amizade_Recomenda_Midia VALUES ('1', 'leticiarina', 'leticia_rina', 'mrpinoquio', 'mr_pinoquio', 'Stranger Things', 'Melhor série Pipi');
+INSERT INTO Amizade_Recomenda_Midia VALUES ('2', 'mrpinoquio', 'mr_pinoquio', 'juregmonteiro', 'jureg_monteiro', 'Lost', 'Assiste aí Jurg, muito bom');
+INSERT INTO Amizade_Recomenda_Midia VALUES ('3', 'kawaiiro', 'kawaiiro', 'gguimaraes', 'g_guimaraes');
+INSERT INTO Amizade_Recomenda_Midia VALUES ('4', 'juregmonteiro', 'jureg_monteiro', 'kawaiiro', 'kawaiiro', 'How I Met Your Mother', 'Marshall homão da porra');
+INSERT INTO Amizade_Recomenda_Midia VALUES ('5', 'gguimaraes', 'g_guimaraes', 'leticiarina', 'leticia_rina');
 
-INSERT INTO Preparador VALUES ('123cdxPP', 'lucassoares1793@gmail.com', '123456789aA!');
-INSERT INTO Preparador VALUES ('321xdcPP', 'xofanna@gimail.com', '123456789aA@');
-INSERT INTO Preparador VALUES ('70r741PP', 'tortao@jureg.jurg', '123456789aA#');
+INSERT INTO Temporada VALUES ('1', 'Friends', '24');
+INSERT INTO Temporada VALUES ('6', 'How I Met Your Mother', '24');
+INSERT INTO Temporada VALUES ('1', 'Stranger Things', '8');
+INSERT INTO Temporada VALUES ('3', 'How to Get Away with Murder', '15');
+INSERT INTO Temporada VALUES ('5', 'Lost', '17');
 
-INSERT INTO Modalidade VALUES (1, 'Arremeço de peso', 'O objetivo é arremessar o mais longe possível uma bola de metal maciço. Para os homens, o peso tem 7,260 kg; para as mulheres, 4 kg. No arremesso, o atleta mantém o braço flexionado, apoiando o peso junto ao pescoço. Deve permanecer dentro de um círculo com 2,1 metros de diâmetro.');
-INSERT INTO Modalidade VALUES (2, '100 Metros Borboleta', '100 Metros Borboleta é a modalidade de velocidade do estilo mariposa da natação.');
-INSERT INTO Modalidade VALUES (3, 'Salto triplo', 'O Salto Triplo é uma combinação de três saltos sucessivos que terminam com a queda numa caixa de areia. A prova inicia-se com uma corrida de impulso. ');
+INSERT INTO Midia_Pessoa VALUES ('Harry Potter and the Half-Blood Prince', 'Daniel Radcliffe', '0', '1');
+INSERT INTO Midia_Pessoa VALUES ('The Breakfast Club', 'John Hughes', '1', '0');
+INSERT INTO Midia_Pessoa VALUES ('Clube da Luta', 'Brad Pitt', '0', '1');
+INSERT INTO Midia_Pessoa VALUES ('Titanic', 'Leonardo DiCaprio', '0', '1');
+INSERT INTO Midia_Pessoa VALUES ('Moonlight', 'Barry Jenkins', '1', '0');
 
-INSERT INTO Nacao VALUES ('Brasil', 'America do Sul', 50, NULL, NULL, NULL);
-INSERT INTO Nacao VALUES ('Alemanha', 'Europa', 60, 'Futebol', NULL, NULL);
-INSERT INTO Nacao VALUES ('Estados Unidos', 'America do Norte', 75, 'Basquete', NULL, NULL);
+INSERT INTO Serie_Pessoa VALUES ('Friends', 'Jennifer Aniston', '0', '1');
+INSERT INTO Serie_Pessoa VALUES ('How I Met Your Mother', 'Josh Radnor', '0', '1');
+INSERT INTO Serie_Pessoa VALUES ('Stranger Things', 'The Duffer Brothers', '1', '0');
+INSERT INTO Serie_Pessoa VALUES ('How to Get Away with Murder', 'Peter Nowalk', '1', '0');
+INSERT INTO Serie_Pessoa VALUES ('Lost', 'Jeffrey Lieber', '1', '0');
 
-INSERT INTO Atleta VALUES ('321xdcPP', 1, 'Brasil', '123cdxAA', 100.0, 2.00, 1, 0);
-INSERT INTO Atleta VALUES ('70r741PP', 2, 'Alemanha', '321xdcAA', 80.0, 1.80, 1, 1);
-INSERT INTO Atleta VALUES ('123cdxPP', 3, 'Estados Unidos', '70r741AA', 120.0, 1.70, 1, 0);	
-INSERT INTO Atleta VALUES ('321xdcPP', 1, 'Brasil', '623cdxAA', 100.0, 2.00, 1, 0);
-INSERT INTO Atleta VALUES ('70r741PP', 2, 'Alemanha', '621xdcAA', 80.0, 1.80, 0, 1);
-INSERT INTO Atleta VALUES ('123cdxPP', 3, 'Estados Unidos', '60r741AA', 120.0, 1.70, 1, 0);	
+INSERT INTO Pessoa_Participa_Midia VALUES ('Harry Potter and the Half-Blood Prince', 'Bonnie Wright', '0', '1');
+INSERT INTO Pessoa_Participa_Midia VALUES ('The Breakfast Club', 'Carl Reed', '0', '1');
+INSERT INTO Pessoa_Participa_Midia VALUES ('Clube da Luta', 'Art Linson', '1', '0');
+INSERT INTO Pessoa_Participa_Midia VALUES ('Titanic', 'Jon Landau', '1', '0');
+INSERT INTO Pessoa_Participa_Midia VALUES ('Moonlight', 'Jaden Piner', '0', '1');
 
-INSERT INTO TelefonePessoa VALUES ('123cdxPP', 33334444);
-INSERT INTO TelefonePessoa VALUES ('321xdcPP', 12345678);
-INSERT INTO TelefonePessoa VALUES ('70r741PP', 87654321);
-INSERT INTO TelefonePessoa VALUES ('123cdxAA', 12312312);
-INSERT INTO TelefonePessoa VALUES ('321xdcAA', 66666666);
-INSERT INTO TelefonePessoa VALUES ('70r741AA', 25252525);
-INSERT INTO TelefonePessoa VALUES ('623cdxAA', 00312312);
-INSERT INTO TelefonePessoa VALUES ('621xdcAA', 00666666);
-INSERT INTO TelefonePessoa VALUES ('60r741AA', 00252525);
+INSERT INTO Pessoa_Participa_Serie VALUES ('Friends', 'Kevin S. Bright', '1', '0');
+INSERT INTO Pessoa_Participa_Serie VALUES ('How I Met Your Mother', 'Michael J. Fox', '0', '1');
+INSERT INTO Pessoa_Participa_Serie VALUES ('Stranger Things', 'Cindy Holland', '1', '0');
+INSERT INTO Pessoa_Participa_Serie VALUES ('How to Get Away with Murder', 'Katie Findlay', '0', '1');
+INSERT INTO Pessoa_Participa_Serie VALUES ('Lost', 'J. J. Abrams', '1', '0');
 
-INSERT INTO Medico VALUES ('123456789aaa', 396440216, 'Airo medico', 'Guarulhos', 'São Paulo', 'Brasil');
-INSERT INTO Medico VALUES ('987654321bbb', 825500244, 'Gi medico', 'Ribeirão Preto', 'São Paulo', 'Brasil');
-INSERT INTO Medico VALUES ('123987456ccc', 111222355, 'Jurg medico', 'São Paulo', 'São Paulo', 'Brasil');
-
-INSERT INTO TelefoneMedico VALUES ('123456789aaa', 44443333);
-INSERT INTO TelefoneMedico VALUES ('987654321bbb', 22224444);
-INSERT INTO TelefoneMedico VALUES ('123987456ccc', 12121212);
-
-INSERT INTO RotinaTreino VALUES (1, '123cdxPP', 180);
-INSERT INTO RotinaTreino VALUES (2, '321xdcPP', 240);
-INSERT INTO RotinaTreino VALUES (3, '70r741PP', 150);
-
-INSERT INTO DiasTreino VALUES (1, '123cdxPP', 'Sunday');
-INSERT INTO DiasTreino VALUES (2, '321xdcPP', 'Monday');
-INSERT INTO DiasTreino VALUES (3, '70r741PP', 'Friday');
-
-INSERT INTO Preparo VALUES (1, 'O atleta realiza cerca de 10m de alongamento e 30m de corrida para aquecimento antes de qualquer treino.');
-INSERT INTO Preparo VALUES (2, 'O atleta realiza cerca de 15m de alongamento e 20m de corrida para aquecimento antes de qualquer treino.');
-INSERT INTO Preparo VALUES (3, 'O atleta realiza cerca de 20m de alongamento e 40m de corrida para aquecimento antes de qualquer treino.');
-
-INSERT INTO Recuperacao VALUES (1, 'O Atleta realizou 10 sessões de fisioterapia');
-INSERT INTO Recuperacao VALUES (2, 'O Atleta realizou 15 sessões com o psicólogo');
-INSERT INTO Recuperacao VALUES (3, 'O Atleta permaneceu afastado, em repouso, por um período de um mês');
-
-INSERT INTO Treino VALUES (1, 'Musculação');
-INSERT INTO Treino VALUES (2, 'Agachamento');
-INSERT INTO Treino VALUES (3, 'Flexões');
-
-INSERT INTO PreparoRotina VALUES (1, '123cdxPP', 1);
-INSERT INTO PreparoRotina VALUES (2, '321xdcPP', 2);
-INSERT INTO PreparoRotina VALUES (3, '70r741PP', 3);
-
-INSERT INTO RecuperacaoRotina VALUES (1, '123cdxPP', 1);
-INSERT INTO RecuperacaoRotina VALUES (2, '321xdcPP', 2);
-INSERT INTO RecuperacaoRotina VALUES (3, '70r741PP', 3);
-
-INSERT INTO TreinoRotina VALUES (1, '123cdxPP', 1);
-INSERT INTO TreinoRotina VALUES (2, '321xdcPP', 2);
-INSERT INTO TreinoRotina VALUES (3, '70r741PP', 3);
-
-INSERT INTO TesteDoping VALUES (1, 'Exame de sangue', 0);
-INSERT INTO TesteDoping VALUES (2, 'Exame de urina', 0);
-INSERT INTO TesteDoping VALUES (3, 'Exame de sangue e urina', 1);
-
-INSERT INTO TestarDoping VALUES ('123456789aaa', '123cdxAA', 1);
-INSERT INTO TestarDoping VALUES ('987654321bbb', '321xdcAA', 2);
-INSERT INTO TestarDoping VALUES ('123987456ccc', '70r741AA', 3);
-
-INSERT INTO MetodoTratamento VALUES (1, 'Ingerir medicação receitada', 'Muito efetivo');
-INSERT INTO MetodoTratamento VALUES (2, 'Repouso', 'Pouco Efetivo');
-INSERT INTO MetodoTratamento VALUES (3, 'Placebo', 'Inútil');
-
-INSERT INTO Diagnostico VALUES (1, 'TODO');
-INSERT INTO Diagnostico VALUES (2, 'TODO');
-INSERT INTO Diagnostico VALUES (3, 'TODO');
-INSERT INTO Diagnostico VALUES (4, 'TODO');
-INSERT INTO Diagnostico VALUES (5, 'TODO');
-INSERT INTO Diagnostico VALUES (6, 'TODO');
-INSERT INTO Diagnostico VALUES (7, 'TODO');
-INSERT INTO Diagnostico VALUES (8, 'TODO');
-INSERT INTO Diagnostico VALUES (9, 'TODO');
-INSERT INTO Diagnostico VALUES (10, 'TODO');
-INSERT INTO Diagnostico VALUES (11, 'TODO');
-
-INSERT INTO Consulta VALUES ('08-AUG-2017', '123cdxAA', '123456789aaa', 1, 'Quadro não urgente');
-INSERT INTO Consulta VALUES ('13-AUG-2017', '123cdxAA', '123456789aaa', 2, 'Quadro não urgente');
-INSERT INTO Consulta VALUES ('20-AUG-2017', '123cdxAA', '123456789aaa', 3, 'Quadro não urgente');
-INSERT INTO Consulta VALUES ('21-AUG-2017', '123cdxAA', '987654321bbb', 4, 'Quadro não urgente');
-INSERT INTO Consulta VALUES ('12-AUG-2017', '321xdcAA', '987654321bbb', 5, 'Quadro urgente');
-INSERT INTO Consulta VALUES ('22-AUG-2017', '321xdcAA', '987654321bbb', 6, 'Quadro urgente');
-INSERT INTO Consulta VALUES ('30-JUL-2017', '70r741AA', '123987456ccc', 7, 'Quadro não urgente');
-INSERT INTO Consulta VALUES ('18-AUG-2017', '623cdxAA', '123456789aaa', 8, 'Quadro não urgente');
-INSERT INTO Consulta VALUES ('20-AUG-2017', '623cdxAA', '123987456ccc', 9, 'Quadro não urgente');
-INSERT INTO Consulta VALUES ('13-AUG-2017', '621xdcAA', '987654321bbb', 10, 'Quadro urgente');
-INSERT INTO Consulta VALUES ('29-JUL-2017', '60r741AA', '123987456ccc', 11, 'Quadro não urgente');
-
-INSERT INTO Atendimento VALUES ('123cdxAA', '123456789aaa', '08-AUG-2017');
-INSERT INTO Atendimento VALUES ('123cdxAA', '123456789aaa', '13-AUG-2017');
-INSERT INTO Atendimento VALUES ('123cdxAA', '123456789aaa', '20-AUG-2017');
-INSERT INTO Atendimento VALUES ('123cdxAA', '987654321bbb', '21-AUG-2017');
-INSERT INTO Atendimento VALUES ('321xdcAA', '987654321bbb', '12-AUG-2017');
-INSERT INTO Atendimento VALUES ('321xdcAA', '987654321bbb', '22-AUG-2017');
-INSERT INTO Atendimento VALUES ('70r741AA', '123987456ccc', '30-JUL-2017');
-INSERT INTO Atendimento VALUES ('623cdxAA', '123456789aaa', '18-AUG-2017');
-INSERT INTO Atendimento VALUES ('623cdxAA', '123987456ccc', '20-AUG-2017');
-INSERT INTO Atendimento VALUES ('621xdcAA', '987654321bbb', '13-AUG-2017');
-INSERT INTO Atendimento VALUES ('60r741AA', '123987456ccc', '29-JUL-2017');
-
-INSERT INTO Tratamento VALUES (1, 1);
-INSERT INTO Tratamento VALUES (2, 2);
-INSERT INTO Tratamento VALUES (3, 3);
-
-INSERT INTO Lesao VALUES (1, 'Fratura no fêmur');
-INSERT INTO Lesao VALUES (2, 'Rompimento do tendão');
-INSERT INTO Lesao VALUES (3, 'Fratura no pé');
-
-INSERT INTO LesaoMedico VALUES (1, '123456789aaa');
-INSERT INTO LesaoMedico VALUES (2, '987654321bbb');
-INSERT INTO LesaoMedico VALUES (3, '123987456ccc');
-
-INSERT INTO LesaoAtleta VALUES (1, '123cdxAA');
-INSERT INTO LesaoAtleta VALUES (2, '321xdcAA');
-INSERT INTO LesaoAtleta VALUES (3, '70r741AA');
-
-INSERT INTO Sintoma VALUES ('08-AUG-2017', '123456789aaa', '123cdxAA', 'Tosse');
-INSERT INTO Sintoma VALUES ('08-AUG-2017', '123456789aaa', '123cdxAA', 'Febre');
-INSERT INTO Sintoma VALUES ('30-JUL-2017', '123987456ccc', '70r741AA', 'Dor no corpo');
-INSERT INTO Sintoma VALUES ('08-AUG-2017', '123456789aaa', '123cdxAA', 'Dor de Cabeça');
-INSERT INTO Sintoma VALUES ('20-AUG-2017', '123456789aaa', '123cdxAA', 'Nenhum');
-INSERT INTO Sintoma VALUES ('21-AUG-2017', '987654321bbb', '123cdxAA', 'Doença X');
-INSERT INTO Sintoma VALUES ('12-AUG-2017', '987654321bbb', '321xdcAA', 'Inflamação');
-INSERT INTO Sintoma VALUES ('22-AUG-2017', '987654321bbb', '321xdcAA', 'Doença Y');
-INSERT INTO Sintoma VALUES ('30-JUL-2017', '123987456ccc', '70r741AA', 'Doença X');
-INSERT INTO Sintoma VALUES ('18-AUG-2017', '123456789aaa', '623cdxAA', 'Doença X');
-INSERT INTO Sintoma VALUES ('20-AUG-2017', '123987456ccc', '623cdxAA', 'Doença X');
-INSERT INTO Sintoma VALUES ('13-AUG-2017', '987654321bbb', '621xdcAA', 'Doença Y');
-INSERT INTO Sintoma VALUES ('29-JUL-2017', '123987456ccc', '60r741AA', 'Doença X');
-
-COMMIT;
+INSERT INTO Perfil VALUES ('leticiarina', 'leticia_rina', '18', 'Full HD', 'Japonês', 'Inglês');
+INSERT INTO Perfil VALUES ('mrpinoquio', 'mr_pinoquio', '18', 'HD', 'Português', 'Inglês');
+INSERT INTO Perfil VALUES ('juregmonteiro', 'jureg_monteiro', '18', 'HD', 'Inglês', 'Português');
+INSERT INTO Perfil VALUES ('kawaiiro', 'kawaiiro', '18', 'Low quality', 'Inglês', 'Espanhol');
+INSERT INTO Perfil VALUES ('gguimaraes', 'g_guimaraes', '18', 'Normal', 'Espanhol', 'Inglês');
